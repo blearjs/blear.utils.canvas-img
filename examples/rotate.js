a609f4322b013b1f43dbffad593cd5580a70450a
#云淡然 <cloudcome@163.com>
2018-06-28 19:27:33 +0800

    ~ 图片旋转定位

--- examples/rotate.js
+++ examples/rotate.js
@@ -12,6 +12,10 @@ var canvasImg = require('../src/index');
 var canvasEl = document.getElementById('canvas');
 var urlEl = document.getElementById('url');
 var drawEl = document.getElementById('draw');
+var rotate1El = document.getElementById('rotate1');
+var rotate2El = document.getElementById('rotate2');
+var rotate3El = document.getElementById('rotate3');
+var rotate4El = document.getElementById('rotate4');
 
 
 drawEl.onclick = function () {
@@ -19,25 +23,37 @@ drawEl.onclick = function () {
     urlEl.disabled = true;
 
     var imgEl = new Image();
+
     imgEl.src = urlEl.value;
     imgEl.onload = function () {
         drawEl.disabled = false;
         urlEl.disabled = false;
-
+        canvasEl.width = imgEl.width;
+        canvasEl.height = imgEl.height;
         canvasImg.draw(canvasEl, imgEl, {
-            srcLeft: -100,
-            srcTop: -300,
             srcWidth: imgEl.width,
-            srcHeight: imgEl.height,
-            drawLeft: 0,
-            drawTop: 0,
-            drawWidth: 400,
-            drawHeight: 400
+            srcHeight: imgEl.height
         });
     };
     imgEl.onerror = function () {
         alert('图片加载失败');
         drawEl.disabled = false;
         urlEl.disabled = false;
+    };
+
+    rotate1El.onclick = function () {
+        canvasImg.orientate(canvasEl, 90);
+    };
+
+    rotate2El.onclick = function () {
+        canvasImg.orientate(canvasEl, 180);
+    };
+
+    rotate3El.onclick = function () {
+        canvasImg.orientate(canvasEl, 270);
+    };
+
+    rotate4El.onclick = function () {
+        canvasImg.orientate(canvasEl, -90);
     };
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * blear.utils.canvas-img
  * @author ydr.me
  * @create 2016年08月20日10:21:42
+ * @update 2018年06月28日19:27:22
  */
 
 'use strict';
@@ -24,11 +25,12 @@ var defaults = {
 var isEmpty = function (o) {
     return typeis.Null(o) || typeis.Undefined(o);
 };
+var tempCanvasEl = document.createElement('canvas');
 
 
 /**
  * 绘制图像
- * @param canvasEl {Object} 画布
+ * @param canvasEl {HTMLCanvasElement} 画布
  * @param imgEl {HTMLImageElement} 图片对象，必须保证图片已经 onload 了
  * @param [options] {Object} 配置
  * @param [options.srcLeft] {Number} 源横坐标
@@ -40,7 +42,7 @@ var isEmpty = function (o) {
  * @param [options.drawWidth] {Number} 绘制宽度
  * @param [options.drawHeight] {Number} 绘制高度
  */
-exports.draw = function (canvasEl, imgEl, options) {
+var draw = exports.draw = function (canvasEl, imgEl, options) {
     options = object.assign({}, defaults, options);
     var context = canvasEl.getContext('2d');
     var srcLeft = options.srcLeft;
@@ -85,3 +87,78 @@ exports.draw = function (canvasEl, imgEl, options) {
         drawLeft, drawTop, drawWidth, drawHeight);
 };
 
+
+/**
+ * 旋转定向，只支持 0°、90°、180°、270°
+ * @param canvasEl {HTMLCanvasElement} 画布
+ * @param angle {number} 顺时针为正
+ * @param [unResize=false] {Boolean} 不重置尺寸
+ */
+exports.orientate = function (canvasEl, angle, unResize) {
+    var srcWidth = canvasEl.width;
+    var srcHeight = canvasEl.height;
+    var translateX = 0;
+    var translateY = 0;
+    angle %= 360;
+
+    if (angle < 0) {
+        angle += 360;
+    }
+
+    var radian = angle * Math.PI / 180;
+    var context = tempCanvasEl.getContext('2d');
+    var actualWidth = 0;
+    var actualHeight = 0;
+    var drawWidth = 0;
+    var drawHeight = 0;
+
+    switch (angle) {
+        case 0:
+            actualWidth = srcWidth;
+            actualHeight = srcHeight;
+            break;
+
+        case 90:
+            translateX = srcHeight;
+            actualWidth = srcHeight;
+            actualHeight = srcWidth;
+            break;
+
+        case 180:
+            translateX = srcWidth;
+            translateY = srcHeight;
+            actualWidth = srcWidth;
+            actualHeight = srcHeight;
+            break;
+
+        case 270:
+            translateY = srcWidth;
+            actualWidth = srcHeight;
+            actualHeight = srcWidth;
+            break;
+
+        default:
+            throw new Error('不支持非 0/90/180/270 度数旋转定向');
+    }
+
+    tempCanvasEl.width = actualWidth;
+    tempCanvasEl.height = actualHeight;
+    context.save();
+    context.translate(translateX, translateY);
+    context.rotate(radian);
+    draw(tempCanvasEl, canvasEl, {
+        srcWidth: srcWidth,
+        srcHeight: srcHeight
+    });
+    context.restore();
+
+    if (!unResize) {
+        canvasEl.width = actualWidth;
+        canvasEl.height = actualHeight;
+    }
+
+    draw(canvasEl, tempCanvasEl, {
+        srcWidth: actualWidth,
+        srcHeight: actualHeight
+    });
+};
